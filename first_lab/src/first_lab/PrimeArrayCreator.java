f2279455b3073d48325a0e8a44e2a1c76796411b
Сергей Полозов <fresher40@gmail.com>
2017-10-28 19:14:01 +0300

    Доделана реализация класса PrimeCreator

--- first_lab/src/first_lab/PrimeArrayCreator.java
+++ first_lab/src/first_lab/PrimeArrayCreator.java
@@ -5,16 +5,29 @@ public class PrimeArrayCreator {
 
 	// MARK: - Properties
 
+	private String errorText = "введена размерность, при которой значения внутри массива превышают [2:10^9]";
 	private int begingOfRange = 2;
-	private int endOfRange = 1000000000; // 1 000 000 000
+	private int endOfRange = 100;
 
 	// MARK: - Internal helpers
 
-//	public int getRandomPrime() {
-//		int randomNum = ThreadLocalRandom.current().nextInt(begingOfRange, endOfRange);
-//		return randomNum;
-//	}
+	public int getRandomPrime() {
+		int randomNum = ThreadLocalRandom.current().nextInt(begingOfRange, endOfRange);
+		if (isPrime(randomNum)) {
+			return randomNum;
+		} else {
+			int nextPrime = getNext(randomNum);
+			int previousPrime = getPrevious(randomNum);
+			if (nextPrime != -1) {
+				return nextPrime;
+			} else if (previousPrime != -1) {
+				return previousPrime;
+			}
+		}
+		return -1;
+	}
 
+	// Throws
 	public int[] getRandomArray(int length) {
 		int[] randomArray = new int[length];
 		int fillingCellNumber = 0;
@@ -24,8 +37,7 @@ public class PrimeArrayCreator {
 				randomArray[fillingCellNumber] = begingOfRange;
 			} else {
 				randomArray[fillingCellNumber] = getNext(randomArray[fillingCellNumber-1]);
-				if (randomArray[fillingCellNumber] > 100) throw new IllegalArgumentException(
-						"введена размерность, при которой значения внутри массива превышают [2:10^9]"); 
+				if (randomArray[fillingCellNumber] == -1) throw new IllegalArgumentException(errorText); 
 			}
 			fillingCellNumber += 1;
 		}
@@ -42,7 +54,18 @@ public class PrimeArrayCreator {
 				return nextPrime;
 			}
 		}
-		return 0;
+		return -1;
+	}
+
+	private int getPrevious(int arg) {
+		int previousPrime;
+		for (int i = arg; i > begingOfRange; i--) {
+			previousPrime = i-1;
+			if (isPrime(previousPrime)) {
+				return previousPrime;
+			}
+		}
+		return -1;
 	}
 
 	private boolean isPrime(int arg) {
