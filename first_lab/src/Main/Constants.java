7e66b2b884df3ae0dc7f92d2bc399b06f4c75530
Сергей Полозов <fresher40@gmail.com>
2017-10-30 04:36:06 +0300

    Построена логика работы приложения(присутствует баг со вводом чисел пользователем)

---
+++ first_lab/src/Main/Constants.java
@@ -0,0 +1,21 @@
+package Main;
+
+public class Constants {
+
+	public static String descriptionProgramText = "Днаая программа позволяет получать массив простых чисел "
+			+ "некоторой размерности, которая позволяет вместить числа в диапазоне [2-10^9],"
+			+ "\n"
+			+ "либо случайное простое число этого же диапазона"
+			+ "\n"
+			+ "-----------";
+
+	public static String inputСonditions = "чтобы получить случайное простое число введите 1,"
+			+ "\n"
+			+ "чтобы отобразить массив простых чисел введите 2:";
+
+	public static String divider = "-----------";
+
+	public static String errorInput = "ОШИБКА: Неправильный ввод данных";
+
+	public static String inputLength = "Введите размерность массива";
+}
--- first_lab/src/Main/Main.java
+++ first_lab/src/Main/Main.java
@@ -1,23 +1,47 @@
 package Main;
 
 import Array_creator.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		RandomArrayCreator creator = new RandomPrimeArrayCreator();
-		int i = 25;
-		try {
-			int[] array = creator.getRandomArray(i);
-			for (int h = 0; h<i; h++) {
-				System.out.println(array[h]);
-			}
-		} catch (IllegalArgumentException e) {
-			System.out.println("---");
-			System.out.println(e);
-			System.out.println("---");
-		}
 
-		int prime = creator.getRandomNumber();
-		System.out.println(prime);
+	// MARK: - Main
+
+	public static void main(String[] args) {
+		RandomArrayCreator randomPrimeArrayCreator = new RandomPrimeArrayCreator();
+		Scanner scanner = new Scanner(System.in);
+		String outCondition = "n";
+		String chosenCondition;
+
+		System.out.println(Constants.descriptionProgramText);
+		while (outCondition != "y") {
+			System.out.println(Constants.inputСonditions);
+			chosenCondition = scanner.nextLine();
+			if ( chosenCondition.equals("1") ) {
+				showRandomPrime(randomPrimeArrayCreator, scanner);
+			} else if ( chosenCondition.equals("2") ) {
+				showRandomPrimeArray(randomPrimeArrayCreator, scanner);
+			} else {
+				System.out.println(Constants.errorInput);
+			}
+			System.out.println(Constants.divider);
+		}
+	}
+
+	// MARK: - Private helpers
+
+	private static void showRandomPrime(RandomArrayCreator randomPrimeArrayCreator, Scanner scanner) {
+		int randomNumber = randomPrimeArrayCreator.getRandomNumber();
+		System.out.println("Случайное число: " + randomNumber);
+	}
+
+	private static void showRandomPrimeArray(RandomArrayCreator randomPrimeArrayCreator, Scanner scanner) {
+		System.out.println(Constants.inputLength);
+		if (scanner.hasNextInt()) {
+			int length = scanner.nextInt();
+			System.out.println("\"вывод массива\"");
+		} else {
+			System.out.println(Constants.errorInput);
+		}
 	}
 }
