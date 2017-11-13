ded0d2bd539bcd724a363e4a1f5e278221de01f4
Сергей Полозов <fresher40@gmail.com>
2017-11-13 14:13:49 +0300

    commented RandomArrayCreator

--- first_lab/src/Array_creator/RandomArrayCreator.java
+++ first_lab/src/Array_creator/RandomArrayCreator.java
@@ -1,7 +1,33 @@
 package Array_creator;
 
+/**
+* RandomArrayCreator allow generate random
+* number and array of random numbers of
+* "length" dimension.
+*
+* @author  Polozov Sergey
+* @version 1.0
+* @since   2017-11-12
+*/
+
 public interface RandomArrayCreator {
 
-	public int[] getRandomArray(int length);
+	/**
+	   * Generates random number
+	   * @return int This returns generated random number
+	   */ 
+
 	public int getRandomNumber();
+
+	/**
+	   * This method is used to generate array of 
+	   * numbers of "length" dimension
+	   * 
+	   * @param length This parameter get int value and 
+	   * is dimension of array.
+	   * @return int[] This returns generated array of 
+	   * numbers.
+	   */
+
+	public int[] getRandomArray(int length);
 }
