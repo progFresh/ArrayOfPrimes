fd5860ac4493e08992328c4f7af825134a96ff70
Сергей Полозов <fresher40@gmail.com>
2017-12-08 22:03:39 +0300

    init the first class + delete .gitignore

--- first_lab/src/Main/Main.java
+++ first_lab/src/Main/Main.java
@@ -1,67 +1,82 @@
 package Main;
 
 import Array_creator.*;
-import java.util.Scanner;
-import java.lang.NegativeArraySizeException;
+import javax.swing.*;
 
 public class Main {
-
+	
 	// - Main
-
+	
 	public static void main(String[] args) {
-		RandomArrayCreator randomPrimeArrayCreator = new RandomPrimeArrayCreator();
-		Scanner scanner = new Scanner(System.in);
-		String chosenCondition;
-
-		System.out.println(Constants.descriptionProgramText);
-		while (true) {
-			System.out.println(Constants.inputСonditions);
-			chosenCondition = scanner.nextLine();
-			if ( chosenCondition.equals("1") ) {
-				showRandomPrime(randomPrimeArrayCreator, scanner);
-			} else if ( chosenCondition.equals("2") ) {
-				showRandomPrimeArray(randomPrimeArrayCreator, scanner);
-			} else {
-				System.out.println(Constants.errorInput);
-				setStandbyMode(scanner);
-			}
-			System.out.println(Constants.divider);
-		}
+		JFrame myWindow = new StartWindow();
+		myWindow.setVisible(true);
 	}
-
-	// - Private helpers
-
-	private static void showRandomPrime(RandomArrayCreator randomPrimeArrayCreator, Scanner scanner) {
-		int randomNumber = randomPrimeArrayCreator.getRandomNumber();
-		System.out.println("Случайное число: " + randomNumber);
-		setStandbyMode(scanner);
-	}
-
-	private static void showRandomPrimeArray(RandomArrayCreator randomPrimeArrayCreator, Scanner scanner) {
-		System.out.println(Constants.inputLength);
-		if (scanner.hasNextInt()) {
-			int length = scanner.nextInt();
-			try {
-				int[] primeArray = randomPrimeArrayCreator.getRandomArray(length);
-				System.out.println("Массив:");
-				for (int i = 0; i<length; i++) {
-					System.out.println("a[" + i + "] = " + primeArray[i]);
-				}
-				setStandbyMode(scanner);
-			} catch (IllegalArgumentException exception) {
-				System.out.println(exception);
-				setStandbyMode(scanner);
-			} catch (NegativeArraySizeException negativeSize) {
-				System.out.println(Constants.negaticeSizeException);
-				setStandbyMode(scanner);
-			}	
-		} else {
-			System.out.println(Constants.errorInput);
-			setStandbyMode(scanner);
-		}
-	}
-
-	private static void setStandbyMode(Scanner scanner) {
-		scanner.nextLine();
-	}
+	
+	// Private helpers
 }
+
+
+
+
+
+//public class Main {
+//
+//	// - Main
+//
+//	public static void main(String[] args) {
+//		RandomArrayCreator randomPrimeArrayCreator = new RandomPrimeArrayCreator();
+//		Scanner scanner = new Scanner(System.in);
+//		String chosenCondition;
+//
+//		System.out.println(Constants.descriptionProgramText);
+//		while (true) {
+//			System.out.println(Constants.inputСonditions);
+//			chosenCondition = scanner.nextLine();
+//			if ( chosenCondition.equals("1") ) {
+//				showRandomPrime(randomPrimeArrayCreator, scanner);
+//			} else if ( chosenCondition.equals("2") ) {
+//				showRandomPrimeArray(randomPrimeArrayCreator, scanner);
+//			} else {
+//				System.out.println(Constants.errorInput);
+//				setStandbyMode(scanner);
+//			}
+//			System.out.println(Constants.divider);
+//		}
+//	}
+//
+//	// - Private helpers
+//
+//	private static void showRandomPrime(RandomArrayCreator randomPrimeArrayCreator, Scanner scanner) {
+//		int randomNumber = randomPrimeArrayCreator.getRandomNumber();
+//		System.out.println("Случайное число: " + randomNumber);
+//		setStandbyMode(scanner);
+//	}
+//
+//	private static void showRandomPrimeArray(RandomArrayCreator randomPrimeArrayCreator, Scanner scanner) {
+//		System.out.println(Constants.inputLength);
+//		if (scanner.hasNextInt()) {
+//			int length = scanner.nextInt();
+//			try {
+//				int[] primeArray = randomPrimeArrayCreator.getRandomArray(length);
+//				System.out.println("Массив:");
+//				for (int i = 0; i<length; i++) {
+//					System.out.println("a[" + i + "] = " + primeArray[i]);
+//				}
+//				setStandbyMode(scanner);
+//			} catch (IllegalArgumentException exception) {
+//				System.out.println(exception);
+//				setStandbyMode(scanner);
+//			} catch (NegativeArraySizeException negativeSize) {
+//				System.out.println(Constants.negaticeSizeException);
+//				setStandbyMode(scanner);
+//			}	
+//		} else {
+//			System.out.println(Constants.errorInput);
+//			setStandbyMode(scanner);
+//		}
+//	}
+//
+//	private static void setStandbyMode(Scanner scanner) {
+//		scanner.nextLine();
+//	}
+//}
---
+++ first_lab/src/Main/StartWindow.java
@@ -0,0 +1,11 @@
+package Main;
+
+import javax.swing.*;
+
+public class StartWindow extends JFrame {
+	public StartWindow() {
+		super("Random prime array creator");		
+		this.setBounds(100,100,250,100);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
